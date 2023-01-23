96d77c881da3147b6db8a7af69e529accc18662d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-23 14:06:37 +0100

    bool

--- lezione1/types.php
+++ lezione1/types.php
@@ -6,13 +6,29 @@ $myStringVal ="3.9";
 
 $transoformedVal = intval($myStringVal);
 
+echo floatval($myStringVal);
+echo"<br>";
+$transoformedValNew = round($myStringVal);
+
 echo $transoformedVal;
 echo '<hr>';
 echo $myVal + $myStringVal;
 echo '<hr>';
 echo $myVal . $myStringVal;
 echo '<hr>';
+var_dump((int)8.1);
 
+$x = 8 - 6.4; 
+
+echo (int)$x;
+var_dump($x);
 var_dump($myVal);
 var_dump($myStringVal);
 var_dump($transoformedVal);
+var_dump($transoformedValNew);
+
+echo '<br>*<br>';
+$show_separators = true;
+if (!$show_separators) {
+    echo "<hr>\n";
+}
