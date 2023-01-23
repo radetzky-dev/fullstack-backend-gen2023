c28f5612c299d370512fce0eb198b2a6b9b17a59
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-23 13:56:48 +0100

    intval

--- lezione1/types.php
+++ lezione1/types.php
@@ -2,11 +2,17 @@
 
 $myVal = 3;
 
-$myStringVal ="3.5";
+$myStringVal ="3.9";
 
+$transoformedVal = intval($myStringVal);
+
+echo $transoformedVal;
+echo '<hr>';
 echo $myVal + $myStringVal;
 echo '<hr>';
 echo $myVal . $myStringVal;
+echo '<hr>';
 
 var_dump($myVal);
 var_dump($myStringVal);
+var_dump($transoformedVal);
