96c53b10d22c6bf5c240d3056816a1a8f075fab6
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-23 14:23:32 +0100

    soluzione

--- lezione1/esercizio.php
+++ lezione1/esercizio.php
@@ -9,3 +9,34 @@
 //scrivi una condizione vera che richiama una funzione per stampare il tuo nome
 
 //scrivi i commenti
+
+//concatena due stringhe
+$myName = "Alessia";
+$mySurName = "Grande";
+
+echo $myName ." " . $mySurName;
+
+//fai una somma fra una float e un int
+
+
+echo"<br>";
+$myVal = 3;
+$myFloat = 5.89;
+
+echo $myVal . $myFloat;
+
+
+
+//somma fra int e stringa
+echo"<br>";
+$myNum = 8;
+$myString = "4";
+
+echo $myNum + $myString;
+
+//scrivi una condizione vera che richiama una funzione per stampare il tuo nome
+echo"<br>";
+$myNamePrint = true;
+if ($myNamePrint) {
+    echo "Ciao " .$myName;
+}
