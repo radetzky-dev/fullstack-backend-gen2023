4d9fbef650e1e103b821aef75342e2a2c354521f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-23 11:12:42 +0100

    primo esempio

---
+++ lezione1/esempi.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esempi php</title>
+</head>
+
+<body>
+    <?php
+    $a = 1000;
+    $b = 1200;
+    $c = 1400;
+    $nome = "Paolo";
+    echo "Ciao, ".$nome."<br>";
+    echo "<table border=1 cellspacing=0 cellpading=0>
+<tr> <td><font color=blue>Salario Mario è</td> <td>€ $a </font></td></tr>
+<tr> <td><font color=blue>Salary of Mr. B is</td> <td>$b $</font></td></tr>
+<tr> <td><font color=blue>Salary of Mr. C is</td> <td>$c $</font></td></tr>
+</table>";
+    ?>
+
+</body>
+
+</html>
