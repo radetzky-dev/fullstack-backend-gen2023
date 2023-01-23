175c40a8a7e9a0e309ee4610c56319411bf20ec8
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-23 14:11:06 +0100

    esercizio

---
+++ lezione1/esercizio.php
@@ -0,0 +1,11 @@
+<?php
+
+//concatena due stringhe
+
+//fai una somma fra una float e un int
+
+//somma fra int e var
+
+//scrivi una condizione vera che richiama una funzione per stampare il tuo nome
+
+//scrivi i commenti
