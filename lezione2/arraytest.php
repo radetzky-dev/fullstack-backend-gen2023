0160c5cf878377aa850313a96002c2723ee9a244
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 09:49:22 +0100

    indici

--- lezione2/arraytest.php
+++ lezione2/arraytest.php
@@ -5,3 +5,11 @@ $cars = array ("ferrari","audi","bmw");
 $cars[4] = "toyota";
 $cars[] ="mercedes";
 var_dump($cars);
+
+echo '<hr>';
+//Array associativo
+$ages = array ("Mario" => 32,"Elena" => 27, "Guido" => 29);
+$ages["Pietro"] = 81;
+$ages[] = "Paolo";
+$ages["Maria"] = "16";
+var_dump($ages);
