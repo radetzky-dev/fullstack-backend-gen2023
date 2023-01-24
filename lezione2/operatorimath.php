40c1b8eceace9531e1c4df130900f5faaa41e319
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 12:15:47 +0100

    mate

--- lezione2/operatorimath.php
+++ lezione2/operatorimath.php
@@ -3,14 +3,29 @@
 //esempi operatori aritmetici
 $a = 5;
 $b = 7;
+$numFloat = 7.25;
+
 echo 'Le mie varibili<br>';
 echo "a = $a  e b = $b <br><hr>";
 echo 'a = ' . $a . '  e b = ' . $b . '<br><hr>';
 echo '<br>Somma<br>';
-echo $a + $b;
+echo $a + $b+$numFloat;
 echo '<br>Sottrazione<br>';
-echo $a - $b;
+echo $a - $b-$numFloat;
 echo '<br>Moltiplicazione<br>';
 echo $a * $b;
 echo '<br>Divisione<br>';
-echo $a / $b;
+echo $a / $b+$numFloat;
+
+echo '<br>Modulo (resto) <br>';
+echo 5 % 3;
+echo '<br>';
+echo 4 % 2;
+echo '<br>Incremento<br>';
+$a++;
+$b++;
+echo "a = $a  e b = $b <br><hr>";
+echo '<br>Sottraggo<br>';
+$a--;
+$b--;
+echo "a = $a  e b = $b <br><hr>";
