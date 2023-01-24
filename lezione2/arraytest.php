3eb637b3681e2a035e5dbd2f954ac73f74d05283
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 10:19:49 +0100

    array

--- lezione2/arraytest.php
+++ lezione2/arraytest.php
@@ -34,6 +34,26 @@ $famiglia[] = 15;
 $famiglia[3] =" questo è tre";
 $famiglia[] =" questo è il 6";
 
-var_dump($famiglia);
+var_dump($famiglia);echo '<hr>';
+echo "Stampo Gialli<br>";
+var_dump($famiglia["Gialli"]);
+echo '<hr>';
+echo "Stampo Verdi<br>";
+var_dump($famiglia["Verdi"]);
+echo '<hr>';
+echo "Stampo 4<br>";
+var_dump($famiglia[4]);
+echo '<hr>';
 
 $famiglia["Verdi"] = "d"; //cancella Verdi e inserisce solo d
+
+echo '<hr>';
+$annidato = array (
+    "Rossi" => array ("1","2","3"),
+    "Verdi" => array ("A","b","c"),
+);
+
+$annidato["Rossi"]["secondoLivello"] = array ("1","2",3);
+$annidato["Rossi"]["secondoLivello"]["terzoLivello"] = array ("A","B","C");
+
+var_dump($annidato["Rossi"]);
