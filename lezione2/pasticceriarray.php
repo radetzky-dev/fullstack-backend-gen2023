655c0080a1fa8121192f56bd6f001dfe79e5785f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 11:32:03 +0100

    catalogo

--- lezione2/pasticceriarray.php
+++ lezione2/pasticceriarray.php
@@ -1,37 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pasticceria</title>
 </head>
+
 <body>
     <?php
     $catalogo = array(
         "utensili" => array(
-             "prodotti" => array(
-                 "nome" => "martello",
-                 "prezzo" => 7.99
-             )),
+            "prodotto" =>
+            array(
+                "nome" => "martello",
+                "prezzo" => 7.99
+            ),
+            array(
+                "nome" => "pinza",
+                "prezzo" => 5.99
+            )
+        ),
         "casalinghi" => array(
-            "prodotti" => array(
+            "prodotto" => array(
                 "nome" => "scolapasta",
                 "prezzo" => 1.99
-            )),
-            "giardinaggio" => array(
-                "prodotti" => array(
-                    "nome" => "rastrello",
-                    "prezzo" => 22.75
-                )),
-            );
+            )
+        ),
+        "giardinaggio" => array(
+            "prodotto" => array(
+                "nome" => "rastrello",
+                "prezzo" => 22.75
+            )
+        ),
+    );
 
-            var_dump ($catalogo['utensili']);
-            echo '<hr>';
-            var_dump ($catalogo['casalinghi']);
-            echo '<hr>';
-            var_dump ($catalogo['giardinaggio']);
-            echo '<hr>';
+    var_dump($catalogo['utensili']['prodotto']);
+    echo '<hr>';
+    var_dump($catalogo['casalinghi']);
+    echo '<hr>';
+    var_dump($catalogo['giardinaggio']);
+    echo '<hr>';
 
     //stampa 5 prodotti contenuti in un array con prezzi in una tabella (visualizza immagine)
     //bootstrap per la tabella
@@ -41,6 +51,7 @@
     //    echo " chiave {$key} => {$value} ";
     // }
     ?>
-    
+
 </body>
+
 </html>
