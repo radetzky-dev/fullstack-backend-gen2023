449c657e2bcd237112cba21a8446672e8514f2b7
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 10:58:24 +0100

    esercizio

---
+++ lezione2/pasticceriarray.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pasticceria</title>
+</head>
+<body>
+    <?php
+    //stampa 5 prodotti contenuti in un array con prezzi in una tabella (visualizza immagine)
+    //bootstrap per la tabella
+    //array è multidimensionale e al primo livello categorie: utensili, casalinghi, giardinaggio -> lista di 3 prodotti nome,descriz, prezzo e url immagine 
+
+    //foreach ($mioArray as $key => $value) {
+    //    echo " chiave {$key} => {$value} ";
+    // }
+    ?>
+    
+</body>
+</html>
