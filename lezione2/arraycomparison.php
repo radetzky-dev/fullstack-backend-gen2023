c25ffa67525de3a0fb518594715f856f8e58cb79
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 13:06:11 +0100

    array

--- lezione2/arraycomparison.php
+++ lezione2/arraycomparison.php
@@ -1,4 +1,11 @@
 <?php
+$a = array("a" => "apple", "b" => "banana");
+$b = array("c" => "apple", "d" => "banana");
+$c = $a + $b;
+echo "Union a + b:";
+var_dump($c);
+echo '<hr>';
+
 
 $a = array("a" => "apple", "b" => "banana");
 $b = array("a" => "pear", "b" => "strawberry", "c" => "cherry");
@@ -21,3 +28,31 @@ $a += $b; // Union of $a += $b is $a and $b
 echo "Union of a += b:";
 var_dump($a);
 echo '<hr>';
+
+$a = array("apple", "banana");
+$b = array(1 => "banana", "0" => "apple");
+$c = array(1 => "banana", 0 => "apple");
+$d = array(0 => "apple", 1 => "banana");
+$e = array("mela" => "apple", "ban" => "banana");
+
+echo $a[0]. ' vs '. $d[0].'<br>';
+
+echo $b['0'].'<br>';
+
+echo $e['mela'].'<br>';
+echo $e['ban'].'<br>';
+
+var_dump($a);
+echo '<hr>';
+var_dump($b);
+echo '<hr>';
+var_dump($c);
+echo '<hr>';
+var_dump($d);
+
+echo '<hr>';
+
+var_dump($a == $b); // bool(true)
+var_dump($a === $b); // bool(false)
+echo '<hr>';
+var_dump($a === $d); //true
