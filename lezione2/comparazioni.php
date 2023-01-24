903ee233e2b27b49f50d72d900439f80fcc2dd59
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 12:30:05 +0100

    val

--- lezione2/comparazioni.php
+++ lezione2/comparazioni.php
@@ -8,3 +8,17 @@ var_dump($a == $b);
 var_dump($a == $c);
 var_dump($b == $c);
 var_dump($a === $b);
+
+$a = 9;
+$b = 8;
+//stampo a video i vriabili dichiarati
+echo 'sono a ' . $a;
+echo '<br>';
+echo 'sono b ' . $b;
+echo '<br>';
+
+if ($a > $b) {
+    echo "a is bigger than b<br>";
+} else {
+    echo "a is smaller than b<br>";
+}
