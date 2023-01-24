c05e243eaa255446153658733d3cce5a84d19deb
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 11:28:37 +0100

    catalogo

--- lezione2/pasticceriarray.php
+++ lezione2/pasticceriarray.php
@@ -8,9 +8,34 @@
 </head>
 <body>
     <?php
+    $catalogo = array(
+        "utensili" => array(
+             "prodotti" => array(
+                 "nome" => "martello",
+                 "prezzo" => 7.99
+             )),
+        "casalinghi" => array(
+            "prodotti" => array(
+                "nome" => "scolapasta",
+                "prezzo" => 1.99
+            )),
+            "giardinaggio" => array(
+                "prodotti" => array(
+                    "nome" => "rastrello",
+                    "prezzo" => 22.75
+                )),
+            );
+
+            var_dump ($catalogo['utensili']);
+            echo '<hr>';
+            var_dump ($catalogo['casalinghi']);
+            echo '<hr>';
+            var_dump ($catalogo['giardinaggio']);
+            echo '<hr>';
+
     //stampa 5 prodotti contenuti in un array con prezzi in una tabella (visualizza immagine)
     //bootstrap per la tabella
-    //array è multidimensionale e al primo livello categorie: utensili, casalinghi, giardinaggio -> lista di 3 prodotti nome,descriz, prezzo e url immagine 
+    //array è multidimensionale e al primo livello categorie: utensili, casalinghi, giardinaggio -> lista di 5 prodotti nome,descriz, prezzo e url immagine 
 
     //foreach ($mioArray as $key => $value) {
     //    echo " chiave {$key} => {$value} ";
