bf5d4178d8075b742cb8b7eb810b809b8ffaafdf
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 12:11:48 +0100

    operazioni

---
+++ lezione2/operatorimath.php
@@ -0,0 +1,16 @@
+<?php
+
+//esempi operatori aritmetici
+$a = 5;
+$b = 7;
+echo 'Le mie varibili<br>';
+echo "a = $a  e b = $b <br><hr>";
+echo 'a = ' . $a . '  e b = ' . $b . '<br><hr>';
+echo '<br>Somma<br>';
+echo $a + $b;
+echo '<br>Sottrazione<br>';
+echo $a - $b;
+echo '<br>Moltiplicazione<br>';
+echo $a * $b;
+echo '<br>Divisione<br>';
+echo $a / $b;
