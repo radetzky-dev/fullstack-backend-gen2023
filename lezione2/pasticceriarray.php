8a7336013205f791e36609cfd22984e1dfdfc555
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-24 14:26:37 +0100

    finito

--- lezione2/pasticceriarray.php
+++ lezione2/pasticceriarray.php
@@ -48,22 +48,38 @@ function showCategory(array $catalogo, string $catName): void
             $qta = checkIfLast($prodotti['qta']);
             $price = $prodotti['prezzo'];
 
-            if ($qta == "ULTIMO")
-            {
-                $price = calculateDiscount ($price,10);
+            if ($qta == "ULTIMO") {
+                $oldPrice = "<del>" . $price . "</del>";
+                $price = calculateDiscount($price, 15);
+                $price = $oldPrice . '<br><b>' . $price . '</b>';
+                $qta = "<b>ULTIMO</b>";
             }
 
-            echo "<tr><td>" . $qta. "</td><td>" . $prodotti['nome'] . "</td><td>" . $price . " €" . "</td><td>" . $catName . "</td>
-            <td><button class='btn btn-primary'>Compra</button></td>
+            $buttonStatus = "";
+            if ($qta == "ESAURITO") {
+                $qta = "<span class='text-danger'>ESAURITO</span>";
+                $buttonStatus = "disabled";
+            }
+
+            echo "<tr><td>" . $qta . "</td><td>" . $prodotti['nome'] . "</td><td>" . $price . " €" . "</td><td>" . $catName . "</td>
+            <td><button class='btn btn-primary' " . $buttonStatus . " >Compra</button></td>
             </tr>";
         }
     }
 }
 
-function calculateDiscount ($price, $percentDiscount)
+
+/**
+ * calculateDiscount
+ *
+ * @param  mixed $price
+ * @param  mixed $percentDiscount
+ * @return float
+ */
+function calculateDiscount($price, $percentDiscount): float
 {
-    //TODO
-    return 0;
+    $sconto = ($price * $percentDiscount) / 100;
+    return round($price - $sconto, 2);
 }
 
 function checkIfLast($qta)
