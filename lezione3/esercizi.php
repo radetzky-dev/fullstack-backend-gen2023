c206a7affe46536fabdaf29568b5f239ad058a3f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 10:45:35 +0100

    esercizio1

---
+++ lezione3/esercizi.php
@@ -0,0 +1,7 @@
+<?php
+
+//array con una serie di numeri 67, 3, 0, "pietro", 11
+
+//ciclo da esercizio ieri
+
+//funzione \ check che dica se il numero è positivo, negativo, è zero o non è un numero
