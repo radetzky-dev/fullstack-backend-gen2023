5b8f461139c28819ade6e42050f626c190a54539
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 14:26:22 +0100

    shop

---
+++ shopstore/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "inc/functions.php";
+require_once "data/data.php";
+require_once "inc/header.php";
+require_once "inc/navbar.php";  //NAV BAR DA SISTEMARE
+?>
+<h3>Welcome in our shop</h3>
+<?php
+
+require_once "inc/footer.php";
