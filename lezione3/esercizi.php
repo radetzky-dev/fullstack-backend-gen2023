260d40176f47ba9170f6c22aff9db1ee92b6214a
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 11:35:38 +0100

    finale

--- lezione3/esercizi.php
+++ lezione3/esercizi.php
@@ -8,14 +8,29 @@
  */
 function checkPosNeg($numero)
 {
-    if ($numero >= 18) {
-        echo "Il numero è positivo e puoi prendere la patente";
+    if ($numero > 0) {
+        echo "Il numero è positivo";
     } elseif ($numero < 0) {
         echo "il numero è negativo";
     } elseif ($numero < 18) {
-     echo "Il numero è postivo";
+        echo "Il numero è postivo";
     } else {
         echo "il numero è uguale a 0";
+    }
+}
+
+/**
+ * checkPatente
+ *
+ * @param  mixed $numero
+ * @return void
+ */
+function checkPatente($numero)
+{
+    if ($numero >= 18) {
+        echo " Puoi prendere la patente";
+    } elseif ($numero >= 0) {
+        echo " Non puoi prendere la patente";
     }
 }
 
@@ -25,8 +40,11 @@ $myNum = array(67, -3, 0, "Pietro", 11, 22.4, -13.2, "Guido", 0, 81);
 
 foreach ($myNum as $key => $numero) {
     echo "Analizzo " . $numero . ": ";
-    if (is_numeric($numero) && is_int($numero)) {
+    if (is_numeric($numero)) {
         checkPosNeg($numero);
+        if (is_int($numero)) {
+            checkPatente($numero);
+        }
     }
     echo "<br>";
 }
