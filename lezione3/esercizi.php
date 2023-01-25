d976ba993614c7203b8205d1fd6a668ed868463e
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 11:10:18 +0100

    risultato

--- lezione3/esercizi.php
+++ lezione3/esercizi.php
@@ -2,6 +2,25 @@
 
 //array con una serie di numeri 67, -3, 0, "pietro", 11
 
-//ciclo da esercizio ieri
+function checkNumber($numero)
+{
+    if (is_numeric($numero)) {
+        if ($numero > 0) {
+            echo "il numero è positivo";
+        } elseif ($numero < 0) {
+            echo "il numero è negativo";
+        } else {
+            echo "il numero è uguale a 0";
+        }
+    } else {
+        echo "Non è un numero!";
+    }
+}
 
-//funzione \ check che dica se il numero è positivo, negativo, è zero o non è un numero
+$myNum = array(67, -3, 0, "Pietro", 11);
+
+foreach ($myNum as $key => $numero) {
+    echo "Analizzo " . $numero . ": ";
+    checkNumber($numero);
+    echo "<br>";
+}
