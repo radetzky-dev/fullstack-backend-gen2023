99e4ea78d03c54e17664dc87f13b100c0b636f6c
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 10:13:44 +0100

    else

--- lezione3/strutturecondizionali.php
+++ lezione3/strutturecondizionali.php
@@ -1,6 +1,6 @@
 <?php
-$a = 5;
-$b = 3;
+$a = 8;
+$b = 7;
 
 function bar() {
     echo 'hello bar!<br>';
@@ -14,6 +14,13 @@ if ($a > $b) {
 }
 echo '<hr>';
 
+if ($a > $b) {
+    echo "$a is greater than $b";
+  } else {
+    echo "$a is NOT greater than $b";
+  }
+  echo '<hr>';
+
 if ($b == 3) bar();
 
 echo '<hr>';
@@ -24,5 +31,15 @@ $r = (1 == $v) ? 'Yes' : 'No'; // $r is set to 'Yes'
 $r = (3 == $v) ? 'Yes' : 'No'; // $r is set to 'No'
 
 echo (1 == $v) ? 'Yes' : 'No'; // 'Yes' will be printed
+
+echo '<hr>';
+
+$t = date("H");
+
+if ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
 
 echo '<hr>';
