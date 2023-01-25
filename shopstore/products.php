7c0bbe22a746b5be1ac6451fab3aa7afd79ac404
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 14:27:36 +0100

    push

--- shopstore/products.php
+++ shopstore/products.php
@@ -2,8 +2,12 @@
 require_once "inc/functions.php";
 require_once "data/data.php";
 require_once "inc/header.php";
-require_once "inc/navbar.php";
+require_once "inc/navbar.php";  //NAV BAR DA SISTEMARE
 
 showProductTable($catalogo);
+
+?>
+<p>loren ipsum </p>
+<?php
 
 require_once "inc/footer.php";
