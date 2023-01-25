c1b256ea51df63720ba24c7ca86957c47e81853f
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 13:13:48 +0100

    soluzione

--- lezione3/esercizio2.php
+++ lezione3/esercizio2.php
@@ -19,3 +19,24 @@ for ($i = 0; $i <= 30; $i++) {
     $somma += $i;
 }
 echo $somma;
+
+echo "<hr>";
+
+/**
+ * showAsterisk
+ *
+ * @param  mixed $num
+ * @return void
+ */
+function showAsterisk($num)
+{
+    $result = "";
+    for ($i = 1; $i <= $num; $i++) {
+        $result = $result . '*';
+    }
+    return $result;
+}
+
+for ($i = 1; $i <= 10; $i++) {
+    echo showAsterisk($i) . "<br>";
+}
