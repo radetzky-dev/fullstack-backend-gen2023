2af6f96d8594b1f4653eb23f4693a022e89c84e9
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 10:02:24 +0100

    ternario

--- lezione3/operatoriternari.php
+++ lezione3/operatoriternari.php
@@ -25,3 +25,20 @@ if ($maggiorenne) {
     echo 'Puoi prendere la patente!<br>';
 }
 
+$username = "";
+$messaggio = "Ciao " . ($username ? $username : 'utente');
+
+echo $messaggio.'<br>';
+
+$eta = 19;
+$esame_superato = false;
+
+$patente = ($eta >= 18) ? ($esame_superato ? true : false) : false;
+
+if ($patente)
+{
+    echo "Complimenti puoi guidare l'auto!<br>";
+} else
+{
+    echo "Peccato, continua a prendere il bus<br>";
+}
