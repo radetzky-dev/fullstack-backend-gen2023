b27f0bb51a4389ecdfaf271e7a3676320971dd70
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-25 13:30:56 +0100

    finito es auto

--- lezione3/esercizio2.php
+++ lezione3/esercizio2.php
@@ -40,3 +40,28 @@ function showAsterisk($num)
 for ($i = 1; $i <= 10; $i++) {
     echo showAsterisk($i) . "<br>";
 }
+
+echo "<hr>";
+function showCars($cars)
+{
+    foreach ($cars as $value) {  //solo il valore
+        echo "Marca : ". ucfirst($value) . '<br>';
+    }
+    echo "<hr>";
+}
+
+$cars = ["audi","fiat","mercedes", "ferrari","auto" => "mini"];
+showCars($cars);
+$cars[] ="Bentley";
+showCars($cars);
+unset($cars[0]);
+unset($cars[2]);
+$cars[] ="Lancia";
+unset($cars["auto"]);
+
+echo "<pre>";
+print_r($cars);
+echo "</pre>";
+
+showCars($cars);
+
