1f78675156324e57a40e46509b71a7cc15663b76
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 11:37:35 +0100

    soluzione

--- lezione3/eserciziocapitali.php
+++ lezione3/eserciziocapitali.php
@@ -2,6 +2,18 @@
 
 $catipal = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw");
 
+asort($catipal);
+foreach ($catipal as $key => $val) {
+    echo "La capitale di " .$key. " è " .$val. "<br>";
+}
+echo "<hr>";
+
+ksort($catipal);
+foreach ($catipal as $key => $val) {
+    echo "La capitale di " .$key. " è " .$val. "<br>";
+}
+
+
 /* order by state e capital 
 
 ciclo :
