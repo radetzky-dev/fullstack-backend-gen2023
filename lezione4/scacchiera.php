eab346062395acb7cda06e9f29003673c9a3d3f1
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 13:24:05 +0100

    todo

---
+++ lezione4/scacchiera.php
@@ -0,0 +1,13 @@
+<?php
+/*
+HTML 
+table larghezza 270px  
+(8x8 columns x 60px) 
+bianco e nero
+bgcolor=#FFFFFF
+bgcolor=#000000
+
+<td height=30px width=30px bgcolor=#000000></td>
+
+Modulo %
+*/
