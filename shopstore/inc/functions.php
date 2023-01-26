df6998b5872fab78dea793c0d0de1f557eafb3c4
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 14:18:25 +0100

    good

--- shopstore/inc/functions.php
+++ shopstore/inc/functions.php
@@ -1,4 +1,5 @@
 <?php
+require_once "inc/vars.php";
 
 /**
  * showCategory
--- shopstore/inc/navbar.php
+++ shopstore/inc/navbar.php
@@ -1,63 +1,23 @@
-<nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand fw-bold" href="vetrina.html">MusaShop</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        News
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark">
-                        <li><a class="dropdown-item" href="#">Articoli</a></li>
-                        <li><a class="dropdown-item" href="#">Chi siamo</a></li>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-                    </ul>
-                </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><?= $shopName; ?></a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                 </li>
-                <div class="dropdown">
-                    <button type="button" class="btn btn-outline-success dropdown-toggle" data-bs-toggle="dropdown"
-                        aria-expanded="false" data-bs-auto-close="outside">
-                        Login
-                    </button>
-                    <form class="dropdown-menu p-4">
-                        <div class="mb-3">
-                            <label for="exampleDropdownFormEmail2" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleDropdownFormEmail2"
-                                placeholder="email@example.com">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleDropdownFormPassword2" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleDropdownFormPassword2"
-                                placeholder="Password">
-                        </div>
-                        <div class="mb-3">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-                                <label class="form-check-label" for="dropdownCheck2">
-                                    Remember me
-                                </label>
-                            </div>
-                        </div>
-                        <p class="nav-item">
-                            <a class="nav-link text-primary" href="iscrizione.html">Hai dimenticato la password?</a>
-                        </p>
-                        <button type="submit" class="btn btn-outline-success">Sign in</button>
-                        <hr>
-                        <p>Non sei ancora iscritto?
-                        <p class="nav-item">
-                            <a class="nav-link text-primary" href="iscrizione.html">Iscriviti qui!</a>
-                        </p>
-                    </form>
-                </div>
-                </ul>
-                <span class="navbar-text">
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Cerca">
-                        <button class="btn btn-outline-success" type="submit">Cerca</button>
-                    </form>
-                </span>
-            </div>
-        </nav>
+                <li class="nav-item">
+                    <a class="nav-link" href="products.php">Prodotti</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <span class="navbar-text">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Cerca">
+            <button class="btn btn-outline-success" type="submit">Cerca</button>
+        </form>
+    </span>
+</nav>
