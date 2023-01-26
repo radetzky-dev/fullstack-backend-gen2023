e9b1ade2cbc967e78f45c4b5d93858e86edd43d8
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 11:48:40 +0100

    ref

--- lezione4/funzioni.php
+++ lezione4/funzioni.php
@@ -1,13 +1,35 @@
 <?php
 
-$myVar =1;
+$myVar = 1;
+$myVarStandard = 2;
 
-function doubleValue (int &$value):void
+/**
+ * doubleValueStandard standard
+ *
+ * @param  mixed $value
+ * @return int
+ */
+function doubleValueStandard(int $value): int
 {
-    $value = $value *2;
-  
+    return $value * 2;
 }
 
-echo $myVar."<br>";
+
+
+/**
+ * doubleValue by reference
+ *
+ * @param  mixed $value
+ * @return void
+ */
+function doubleValue(int &$value): void
+{
+    $value = $value * 2;
+}
+
+echo "Ref " . $myVar . "<br>";
+echo "Stand " . $myVarStandard . "<br>";
 doubleValue($myVar);
-echo $myVar."<br>";
+echo "Ref " . $myVar . "<br>";
+$myVarStandard = doubleValueStandard($myVarStandard);
+echo "Stand " . $myVarStandard . "<br>";
