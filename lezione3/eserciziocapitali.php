555f0397cf879de95ca3afe56e3fcc25071cb085
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 11:22:57 +0100

    capitali

---
+++ lezione3/eserciziocapitali.php
@@ -0,0 +1,11 @@
+<?php
+
+$catipal = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw");
+
+/* order by state e capital 
+
+ciclo :
+La capitale di Netherlands è Amsterdam 
+The capital of Greece is Athens 
+The capital of Germany is Berlin
+*/
