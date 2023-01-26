6f0a3a59ff2323809beff77b245f9f9c3feb279d
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-26 13:12:45 +0100

    soluzione

--- lezione4/numeriprimi.php
+++ lezione4/numeriprimi.php
@@ -5,3 +5,20 @@
 //può essere diviso solo per se stesso
 
 //TIP : $a % $b Modulo
+
+function checkIsPrime($number)
+{
+    if ($number == 1) {
+        return "NO";
+    }
+    for ($i = 2; $i <= $number / 2; $i++) {
+        if ($number % $i == 0)
+            return "NO";
+    }
+    return "SI";
+}
+
+
+for ($i = 1; $i < 101; $i++) {
+    echo "Controllo se $i è primo " . checkIsPrime($i) . "<br>";
+}
