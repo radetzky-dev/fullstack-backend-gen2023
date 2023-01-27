b9714fce70a7ed4cce8d6bd79eadbf681ab2d976
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-27 14:12:28 +0100

    finale

--- lezione5/ricevechiamate.php
+++ lezione5/ricevechiamate.php
@@ -3,15 +3,27 @@
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     echo "Ricevuto chiamata POST <br>";
     echo "Nome " . $_POST["name"] . " " . $_POST["surname"];
+    var_dump($_POST);
     echo "<hr>";
+    echo $_SERVER['SERVER_NAME'] . '<br>';
+    echo "Pagina che chiama " . $_SERVER['HTTP_REFERER'] . '<br>';
+    echo "<pre>";
+    var_dump($_SERVER);
+    echo "</pre>";
+    echo "REQUEST<br>";
+    echo "<pre>";
+    var_dump($_REQUEST);
+    echo "</pre>";
 } else {
-    if (isset($_GET["name"])) {
+    if (isset($_GET["name"]) && isset($_GET["surname"])) {
         echo "Ricevuto chiamata GET <br>";
         echo "Nome " . $_GET["name"] . " " . $_GET["surname"];
+        var_dump($_GET);
+
         echo "<hr>";
-    } else
-    {
+    } else {
         echo "Ricevuta chiamata vuota<br>";
+        echo "Query String " . $_SERVER["QUERY_STRING"] . "<br>";
     }
 }
 echo "<a href='getex.php'>Torna a Get</a> |";
