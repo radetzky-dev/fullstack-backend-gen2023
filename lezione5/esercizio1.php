7a08c14571e8ce19f63eeea88de0525f910def89
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-27 11:54:32 +0100

    finale

--- lezione5/esercizio1.php
+++ lezione5/esercizio1.php
@@ -4,13 +4,24 @@
 // Oggi è venerdì 27 gennaio 2023. Sono le 11:xx.
 //Tip: array giorni\mesi
 
+/**
+ * showTodayDate
+ *
+ * @param  mixed $onlyDate
+ * @return string
+ */
+function showTodayDate($onlyDate = true): string
+{
+    $dayWeek = ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom',];
+    $month = ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'];
 
-$dayWeek = ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom',];
-$month = ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'];
+    $today = getdate();
+    $date = $dayWeek[$today['wday'] - 1] . " " . $today['mday'] . " " . $month[$today['mon'] - 1] . " " . $today['year'];
+    if (!$onlyDate) {
+        $date = $date . ' - ' . $today['hours'] . ":" . $today['minutes'];
+    }
+    return $date;
+}
 
-$today = getdate();
-echo "<pre>";
-print_r($today);
-echo "</pre>";
-
-echo "Oggi è ".$dayWeek[$today['wday']-1]. " ". $today['mday']. " ".$month[$today['mon']-1]." ". $today['year']. " .<br>" . "Sono le ". $today['hours'] . " : ". $today['minutes'] . " : " . $today['seconds'] ;
+echo showTodayDate() . '<br>';
+echo showTodayDate(false) . '<br>';
