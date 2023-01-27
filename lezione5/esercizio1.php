910e2e1f2321f907c09a9bd95d8ed49db8638bd7
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-27 11:36:33 +0100

    orario

--- lezione5/esercizio1.php
+++ lezione5/esercizio1.php
@@ -1,5 +1,16 @@
 <?php
 
 //stampare la data e ora del giorno nel formato:
-    // Oggi è venerdì 27 gennaio 2023. Sono le 11:xx.
-    //Tip: array giorni\mesi
+// Oggi è venerdì 27 gennaio 2023. Sono le 11:xx.
+//Tip: array giorni\mesi
+
+
+$dayWeek = ['Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab', 'Dom',];
+$month = ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'];
+
+$today = getdate();
+echo "<pre>";
+print_r($today);
+echo "</pre>";
+
+echo "Oggi è ".$dayWeek[$today['wday']-1]. " ". $today['mday']. " ".$month[$today['mon']-1]." ". $today['year'].  ". Sono le <br>";
