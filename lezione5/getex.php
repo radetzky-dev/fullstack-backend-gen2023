9bb3a404fe6c86a93f1939a15901765bb7e6ddbb
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-27 13:35:16 +0100

    get

---
+++ lezione5/getex.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_GET["name"])) {
+    echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+}
+
+if (isset($_GET["surname"])) {
+    echo 'Cognome' . htmlspecialchars($_GET["surname"]) . '!';
+}
+
+?>
+<p>Test</p>
+<form>
+    <input type="text" id="name" name="name" />
+    <input type="text" id="surname" name="surname" />
+    <input type="submit" value="Invia" />
+</form>
