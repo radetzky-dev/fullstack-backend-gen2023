68c28e66bdcab49625533f7f75638b7dab671dd5
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-27 10:48:49 +0100

    soluzione

--- lezione4/palindromo.php
+++ lezione4/palindromo.php
@@ -3,3 +3,18 @@
 //array: pippo, mario, madam, osso,  ossesso, esose, computer
 
 //funzione che mi dice se la parola è palindroma : tip: spulicare la doc delle stringhe
+
+$words = ["pippo", "mario", "madam", "osso",  "ossesso", "esose", "computer"];
+
+function checkPalindromo(string $myVal)
+{
+    if ($myVal == strrev($myVal)) {
+        return true;
+    }
+    return false;
+}
+
+foreach ($words as $myVal) {
+    echo "Is $myVal palindromo? <br>";
+    echo  checkPalindromo($myVal) ? "Si<br>" : "No<br>";
+}
