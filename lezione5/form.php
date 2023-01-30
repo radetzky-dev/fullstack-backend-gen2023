da3c9de1c768277ecc3885ca2ec8fb1947968834
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 11:46:25 +0100

    finale

--- lezione5/form.php
+++ lezione5/form.php
@@ -16,6 +16,7 @@
             }
         }
     </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
@@ -54,28 +55,39 @@
         $province = $_POST['province'];
     }
     ?>
-    <form id="anagraficaForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Nome: <input type="text" name="nome" id="nome" value="<?= $name ?>" required><br>
-        Cognome: <input type="text" name="cognome" id="cognome" value="<?= $surname ?>" required><br>
-        Età: <input type="number" name="eta" id="eta" value="<?= $eta ?>" required><br>
-        Città<select name="cities[]" id="cities" multiple required>
-            <option value="Milano" <?= checkIfChecked("Milano", $cities)  ?>>Milano</option>
-            <option value="Roma" <?= checkIfChecked("Roma", $cities) ?>>Roma</option>
-            <option value="Lecce" <?= checkIfChecked("Lecce", $cities)  ?>>Lecce</option>
-            <option value="Palermo" <?= checkIfChecked("Palermo", $cities)  ?>>Palermo</option>
-        </select>
-        <br><br>
-        Provincia
-        <input type="checkbox" name="province[]" id="province" value="Lecce" <?= checkIfChecked("Lecce", $province, "checked") ?>>Lecce <br>
-        <input type="checkbox" name="province[]" id="province" value="Roma" <?= checkIfChecked("Roma", $province, "checked") ?>>Roma <br>
-        <input type="checkbox" name="province[]" id="province" value="Napoli" <?= checkIfChecked("Napoli", $province, "checked") ?>>Napoli <br>
-        <input type="checkbox" name="province[]" id="province" value="Milano" <?= checkIfChecked("Milano", $province, "checked") ?>>Milano <br>
+    <div class="container mt-2">
+        <form id="anagraficaForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <div class="row g-3">
+                <div class="col">
+                    Nome: <input type="text" name="nome" id="nome" value="<?= $name ?>" required><br>
+                    Cognome: <input type="text" name="cognome" id="cognome" value="<?= $surname ?>" required><br>
+                    Età: <input type="number" name="eta" id="eta" value="<?= $eta ?>" required><br>
+                </div>
+                <div class="col-md-12">
+                    Città<select name="cities[]" id="cities" multiple required>
+                        <option value="Milano" <?= checkIfChecked("Milano", $cities)  ?>>Milano</option>
+                        <option value="Roma" <?= checkIfChecked("Roma", $cities) ?>>Roma</option>
+                        <option value="Lecce" <?= checkIfChecked("Lecce", $cities)  ?>>Lecce</option>
+                        <option value="Palermo" <?= checkIfChecked("Palermo", $cities)  ?>>Palermo</option>
+                    </select>
+                </div>
+                <br><br>
+                <div class="col">
+                    Provincia <br>
+                    <input type="checkbox" name="province[]" id="province" value="Lecce" <?= checkIfChecked("Lecce", $province, "checked") ?>>Lecce <br>
+                    <input type="checkbox" name="province[]" id="province" value="Roma" <?= checkIfChecked("Roma", $province, "checked") ?>>Roma <br>
+                    <input type="checkbox" name="province[]" id="province" value="Napoli" <?= checkIfChecked("Napoli", $province, "checked") ?>>Napoli <br>
+                    <input type="checkbox" name="province[]" id="province" value="Milano" <?= checkIfChecked("Milano", $province, "checked") ?>>Milano <br>
+                </div>
+                <input type="hidden" name="id" value="<?= $name . ' ' . $surname ?>">
+                <div class="col-12">
+                    <input type="submit" class="btn btn-primary" value="Invia">
 
-        <input type="hidden" name="id" value="<?= $name . ' ' . $surname ?>">
-        <input type="submit" value="Invia">
-
-        <input type="button" value="Pulisci" onclick="cleanForm();">
-    </form>
+                    <input type="button" class="btn btn-primary " value="Pulisci" onclick="cleanForm();">
+                </div>
+        </form>
+    </div>
+    </div>
 </body>
 
 </html>
