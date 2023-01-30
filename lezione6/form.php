8e00c88b0594435c6d9f6e83e2ea12190c4f7e05
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 14:03:25 +0100

    dummy

--- lezione6/form.php
+++ lezione6/form.php
@@ -19,29 +19,20 @@
 //metodo get
 $name = $surname = $company = $email = $phone = "";
 $dummyPhoto = "https://dummyimage.com/300";
+$dummyName = "Name";
+$dummySurname = "Surname";
+$dummyText = "Qui appariranno i tuoi dati personali";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    /*
-    echo "<pre>";
-    var_dump($_REQUEST);
-    echo "</pre>";
-
-    */
-
-    //var_dump($_POST['uploadPhoto']);
-
     if (isset($_POST['uploadPhoto'])) {
-
         if ($_FILES) {
-            //var_dump($_FILES);
             $uploadDir = __DIR__ . '/uploads';
             foreach ($_FILES as $file) {
                 if (UPLOAD_ERR_OK === $file['error']) {
                     $fileName = basename($file['name']);
                     $result = move_uploaded_file($file['tmp_name'], $uploadDir . DIRECTORY_SEPARATOR . $fileName);
                     if ($result) {
-                        // echo '<br>File uploaded TODO MOSTRO LA FOTO qui: ' . $uploadDir . DIRECTORY_SEPARATOR . $fileName;
                         $dummyPhoto = "uploads" . DIRECTORY_SEPARATOR . $fileName;
                     } else {
                         echo '<br>ERRORE NEL CARICAMENTO DELL\'IMMAGINE!';
@@ -55,68 +46,68 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $company = $_POST['societa'];
         $email = $_POST['email'];
         $phone = $_POST['telefono'];
+
+        $dummyName = $name;
+        $dummySurname = $surname;
+        $dummyText = "<ul>
+            <li>$company</li>
+            <li>$email</li>
+            <li>$phone</li>
+        </ul>";
     }
-
-
-
-    /*
-    echo "<pre>";
-    var_dump($_SERVER);
-    echo "</pre>";
-    */
 }
-
-
-
-
 ?>
 
 <body>
 
     <div class="container">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="<?= $dummyPhoto; ?>" id="profilePhoto" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title" id="namesurname">NOME E COGNOME</h5>
-                <p class="card-text">Lista con dati ul - li ecc.</p>
-                <button class="btn btn-primary">Dati corretti: registrami</button>
+        <h3>Registration form</h3>
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="<?= $dummyPhoto; ?>" id="profilePhoto" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title" id="namesurname"><?= $dummyName . ' ' . $dummySurname; ?></h5>
+                        <p class="card-text"><?= $dummyText; ?></p>
+                        <button class="btn btn-primary">Dati corretti: registrami</button>
+                    </div>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" id="FormProfilePhoto">
+                        <input type="file" name="file">
+                        <input type="hidden" name="uploadPhoto" value="uploadPhoto" />
+                        <input type="submit" name="upload" value="Carica foto profilo">
+                    </form>
+                </div>
             </div>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" id="FormProfilePhoto">
-                <input type="file" name="file">
-                <input type="hidden" name="uploadPhoto" value="uploadPhoto" />
-                <input type="submit" name="upload" value="Carica foto profilo">
-            </form>
+            <div class="col">
+                <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="nome">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" value="<?= $name ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="cognome">Cognome</label>
+                        <input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome" value="<?= $surname ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="company">Società</label>
+                        <input type="text" class="form-control" id="societa" name="societa" placeholder="Società" value="<?= $company ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" value="<?= $email ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputTelephone">Telefono</label>
+                        <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono" value="<?= $phone ?>" required>
+                    </div>
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
+                        <label class="form-check-label" for="exampleCheck1">Accetta i nostri termini di servizio</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
-
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" value="<?= $name ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="cognome">Cognome</label>
-                <input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome" value="<?= $surname ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="company">Società</label>
-                <input type="text" class="form-control" id="societa" name="societa" placeholder="Società" value="<?= $company ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" value="<?= $email ?>" required>
-            </div>
-            <div class="form-group">
-                <label for="exampleInputTelephone">Telefono</label>
-                <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono" value="<?= $phone ?>" required>
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
-                <label class="form-check-label" for="exampleCheck1">Accetta i nostri termini di servizio</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-
-
 
     </div>
 </body>
