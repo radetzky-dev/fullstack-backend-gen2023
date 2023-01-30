6731473cb37181dae0c5d6dd7b2cd9eac121b229
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 10:42:53 +0100

    clean form

--- lezione5/form.php
+++ lezione5/form.php
@@ -2,9 +2,15 @@
 
 <head>
     <title>Form test</title>
+    <script>
+        function cleanForm() {
+            let formVal = document.getElementById("anagraficaForm").reset();
+            document.getElementById("nome").setAttribute("value","");
+            document.getElementById("cognome").setAttribute("value","");
+            console.log ("Fatto "+ formVal);
+        }
+    </script>
 </head>
-
-</html>
 
 <body>
     <?php
@@ -16,25 +22,28 @@
     2- mostrare selezione di città e provincia
     3- attivare con javascript la funzione pulisci
     4- mettere boostrap per renderlo più leggibile
+
+    *** extra
+    5 - aggiungere campo eta e controllare che sia numerico
+    6- trasformare questo in una funzione is_integer(array_search('Milano', $cities)) ? 'selected' : null ?
+    
     */
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       var_dump($_POST);
+        var_dump($_POST);
         $name = $_POST['nome'];
         $surname = $_POST['cognome'];
-        /**
-         * <?= array_search('one', $_POST['attributes']) ? null : 'selected'  ?>
-         */
+        $cities = $_POST['cities'];
     }
     ?>
-    <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        Nome: <input type="text" name="nome" value="<?= $name ?>"><br>
-        Cognome: <input type="text" name="cognome" value="<?= $surname ?>"><br>
-        Città<select name="cities[]" multiple>
-            <option value="Milano" >Milano</option>
-            <option value="Roma">Roma</option>
-            <option value="Lecce">Lecce</option>
-            <option value="Palermo">Palermo</option>
+    <form id="anagraficaForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        Nome: <input type="text" name="nome" id="nome" value="<?= $name ?>" required><br>
+        Cognome: <input type="text" name="cognome" id="cognome" value="<?= $surname ?>" required><br>
+        Città<select name="cities[]" multiple required>
+            <option value="Milano" <?= is_integer(array_search('Milano', $cities)) ? 'selected' : null ?>>Milano</option>
+            <option value="Roma" <?= is_integer(array_search('Roma', $cities)) ? 'selected' : null ?>>Roma</option>
+            <option value="Lecce" <?= is_integer(array_search('Lecce', $cities)) ? 'selected' : null ?>>Lecce</option>
+            <option value="Palermo" <?= is_integer(array_search('Palermo', $cities)) ? 'selected' : null ?>>Palermo</option>
         </select>
         <br><br>
         Provincia
@@ -43,8 +52,10 @@
         <input type="checkbox" name="province[]" value="Napoli">Napoli <br>
         <input type="checkbox" name="province[]" value="Milano">Milano <br>
 
-        <input type="hidden" name="id" value="<?= $name.' '.$surname ?>">
-        <input type="submit" value="INVIA">
-        <input type="button" value="Pulisci">
+        <input type="hidden" name="id" value="<?= $name . ' ' . $surname ?>">
+        <input type="submit" value="Invia">
+      
+        <input type="button" value="Pulisci" onclick="cleanForm();">
     </form>
 </body>
+</html>
