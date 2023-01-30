78a31f060d68a8e6ccf6b992bcd453d034360e63
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 11:34:33 +0100

    form

--- lezione5/form.php
+++ lezione5/form.php
@@ -5,57 +5,77 @@
     <script>
         function cleanForm() {
             let formVal = document.getElementById("anagraficaForm").reset();
-            document.getElementById("nome").setAttribute("value","");
-            document.getElementById("cognome").setAttribute("value","");
-            console.log ("Fatto "+ formVal);
+            document.getElementById("nome").setAttribute("value", "");
+            document.getElementById("cognome").setAttribute("value", "");
+            document.getElementById("eta").setAttribute("value", "");
+            document.getElementById("cities").value = "";
+            var textinputs = document.querySelectorAll('input[type=checkbox]');
+            //console.log(textinputs);
+            for (var i = 0; i < textinputs.length; i++) {
+                textinputs[i].checked = false;
+            }
         }
     </script>
 </head>
 
 <body>
     <?php
-    $name = $surname = "";
+    //inizializziamo vuoti
+    $name = $surname = $eta = "";
+    $cities[] = [];
+    $province[] = [];
 
     //TODO
     /*
-    1- fare in modo che tutti i campi siano compilati altrimenti te lo segnala
-    2- mostrare selezione di città e provincia
-    3- attivare con javascript la funzione pulisci
     4- mettere boostrap per renderlo più leggibile
-
-    *** extra
-    5 - aggiungere campo eta e controllare che sia numerico
-    6- trasformare questo in una funzione is_integer(array_search('Milano', $cities)) ? 'selected' : null ?
-    
     */
 
+    /**
+     * checkIfChecked
+     *
+     * @param  mixed $cityName
+     * @param  mixed $list
+     * @param  mixed $checkSign
+     * @return void
+     */
+    function checkIfChecked(string $cityName, array $list, string $checkSign = "selected")
+    {
+        if (is_integer(array_search($cityName, $list))) {
+            return $checkSign;
+        }
+        return null;
+    }
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        var_dump($_POST);
         $name = $_POST['nome'];
         $surname = $_POST['cognome'];
+        $eta = $_POST['eta'];
         $cities = $_POST['cities'];
+        $province = $_POST['province'];
     }
     ?>
     <form id="anagraficaForm" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         Nome: <input type="text" name="nome" id="nome" value="<?= $name ?>" required><br>
         Cognome: <input type="text" name="cognome" id="cognome" value="<?= $surname ?>" required><br>
-        Città<select name="cities[]" multiple required>
-            <option value="Milano" <?= is_integer(array_search('Milano', $cities)) ? 'selected' : null ?>>Milano</option>
-            <option value="Roma" <?= is_integer(array_search('Roma', $cities)) ? 'selected' : null ?>>Roma</option>
-            <option value="Lecce" <?= is_integer(array_search('Lecce', $cities)) ? 'selected' : null ?>>Lecce</option>
-            <option value="Palermo" <?= is_integer(array_search('Palermo', $cities)) ? 'selected' : null ?>>Palermo</option>
+        Età: <input type="number" name="eta" id="eta" value="<?= $eta ?>" required><br>
+        Città<select name="cities[]" id="cities" multiple required>
+            <option value="Milano" <?= checkIfChecked("Milano", $cities)  ?>>Milano</option>
+            <option value="Roma" <?= checkIfChecked("Roma", $cities) ?>>Roma</option>
+            <option value="Lecce" <?= checkIfChecked("Lecce", $cities)  ?>>Lecce</option>
+            <option value="Palermo" <?= checkIfChecked("Palermo", $cities)  ?>>Palermo</option>
         </select>
         <br><br>
         Provincia
-        <input type="checkbox" name="province[]" value="Lecce">Lecce <br>
-        <input type="checkbox" name="province[]" value="Roma">Roma <br>
-        <input type="checkbox" name="province[]" value="Napoli">Napoli <br>
-        <input type="checkbox" name="province[]" value="Milano">Milano <br>
+        <input type="checkbox" name="province[]" id="province" value="Lecce" <?= checkIfChecked("Lecce", $province, "checked") ?>>Lecce <br>
+        <input type="checkbox" name="province[]" id="province" value="Roma" <?= checkIfChecked("Roma", $province, "checked") ?>>Roma <br>
+        <input type="checkbox" name="province[]" id="province" value="Napoli" <?= checkIfChecked("Napoli", $province, "checked") ?>>Napoli <br>
+        <input type="checkbox" name="province[]" id="province" value="Milano" <?= checkIfChecked("Milano", $province, "checked") ?>>Milano <br>
 
         <input type="hidden" name="id" value="<?= $name . ' ' . $surname ?>">
         <input type="submit" value="Invia">
-      
+
         <input type="button" value="Pulisci" onclick="cleanForm();">
     </form>
 </body>
+
 </html>
