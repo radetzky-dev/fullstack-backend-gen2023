c833b2642a47fdd6b52a1144bde16ea9c4ed6ded
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 12:25:12 +0100

    esercizio

---
+++ lezione6/form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<?php
+// 
+/*
+1 grafica bootstrap coi min.js e min.css
+2 inserire campi: nome cognome società qualifica email telefono data di nascita upload della foto
+3 validare campi (no vuoti)
+4 post e carica l'immagine in alto e compila il form coi dati appena inseriti (esercizio precedente)
+
+*/
+?>
+
+<body>
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
