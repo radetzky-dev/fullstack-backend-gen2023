3769f745586e87d99a0213040b327f8c20ed0f1c
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-30 13:40:26 +0100

    modfiiche

--- lezione6/form.php
+++ lezione6/form.php
@@ -17,49 +17,56 @@
 */
 
 $name = $surname = $company = $email = $phone = "";
+$dummyPhoto = "https://dummyimage.com/300";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['nome'];
-    $surname = $_POST['cognome'];
-    $company = $_POST['societa'];
-    $email = $_POST['email'];
-    $phone = $_POST['telefono'];
-}
 
+    /*
+    echo "<pre>";
+    var_dump($_REQUEST);
+    echo "</pre>";
 
-set_time_limit(0);
-ini_set('upload_max_filesize', '500M');
-ini_set('post_max_size', '500M');
-ini_set('max_input_time', 4000); // Play with the values
-ini_set('max_execution_time', 4000); // Play with the values
+    */
 
-if ($_FILES) {
-    echo '<pre>';
-    var_dump($_FILES);
-    echo '<pre>';
-    echo  sys_get_temp_dir();
-    echo '<hr>';
+    //var_dump($_POST['uploadPhoto']);
 
-    //remember :  sudo chmod -R 777 sulla cartella uploads
-    $uploadDir = __DIR__ . '/uploads';
-    echo $uploadDir;
-    echo '<br>';
+    if (isset($_POST['uploadPhoto'])) {
 
-
-    foreach ($_FILES as $file) {
-        if (UPLOAD_ERR_OK === $file['error']) {
-            $fileName = basename($file['name']);
-            echo '<br>->' . $file['tmp_name'];
-            echo '<br>->' . $uploadDir . DIRECTORY_SEPARATOR . $fileName;
-            $result = move_uploaded_file($file['tmp_name'], $uploadDir . DIRECTORY_SEPARATOR . $fileName);
-            if ($result) {
-                echo '<br>File uploaded';
-            } else {
-                echo '<br>ERROR';
+        if ($_FILES) {
+            //var_dump($_FILES);
+            $uploadDir = __DIR__ . '/uploads';
+            foreach ($_FILES as $file) {
+                if (UPLOAD_ERR_OK === $file['error']) {
+                    $fileName = basename($file['name']);
+                    $result = move_uploaded_file($file['tmp_name'], $uploadDir . DIRECTORY_SEPARATOR . $fileName);
+                    if ($result) {
+                        // echo '<br>File uploaded TODO MOSTRO LA FOTO qui: ' . $uploadDir . DIRECTORY_SEPARATOR . $fileName;
+                        $dummyPhoto = "uploads" . DIRECTORY_SEPARATOR . $fileName;
+                    } else {
+                        echo '<br>ERRORE NEL CARICAMENTO DELL\'IMMAGINE!';
+                    }
+                }
             }
         }
+    } else {
+        $name = $_POST['nome'];
+        $surname = $_POST['cognome'];
+        $company = $_POST['societa'];
+        $email = $_POST['email'];
+        $phone = $_POST['telefono'];
     }
+
+
+
+    /*
+    echo "<pre>";
+    var_dump($_SERVER);
+    echo "</pre>";
+    */
 }
+
+
+
 
 ?>
 
@@ -67,55 +74,49 @@ if ($_FILES) {
 
     <div class="container">
         <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="https://dummyimage.com/300" id="profilePhoto" alt="Card image cap">
+            <img class="card-img-top" src="<?= $dummyPhoto; ?>" id="profilePhoto" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title" id="namesurname">NOME E COGNOME</h5>
                 <p class="card-text">Lista con dati ul - li ecc.</p>
-                <button class="btn btn-primary">Registra dati</button>
+                <button class="btn btn-primary">Dati corretti: registrami</button>
             </div>
-
-
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" id="FormProfilePhoto">
+                <input type="file" name="file">
+                <input type="hidden" name="uploadPhoto" value="uploadPhoto" />
+                <input type="submit" name="upload" value="Carica foto profilo">
+            </form>
         </div>
-        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="nome">Nome</label>
                 <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" value="<?= $name ?>" required>
-
             </div>
             <div class="form-group">
                 <label for="cognome">Cognome</label>
                 <input type="text" class="form-control" id="cognome" name="cognome" placeholder="Cognome" value="<?= $surname ?>" required>
-
             </div>
             <div class="form-group">
                 <label for="company">Società</label>
                 <input type="text" class="form-control" id="societa" name="societa" placeholder="Società" value="<?= $company ?>" required>
-
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" value="<?= $email ?>" required>
-
             </div>
             <div class="form-group">
                 <label for="exampleInputTelephone">Telefono</label>
                 <input type="number" class="form-control" id="telefono" name="telefono" placeholder="Telefono" value="<?= $phone ?>" required>
             </div>
-
-            <div class="form-group">
-                <label for="exampleInputTelephone">Carica immagine del profilo</label>
-                <input type="file" name="file" class="form-control" required>
-            </div>
-
-
             <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
                 <label class="form-check-label" for="exampleCheck1">Accetta i nostri termini di servizio</label>
             </div>
-
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+
+
+
     </div>
 </body>
 
