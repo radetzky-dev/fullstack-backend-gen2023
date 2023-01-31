ab054d83ea06b73c578832d2ba13c141adf2b63b
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-31 10:10:30 +0100

    sessioni

--- lezione6/sessioni.php
+++ lezione6/sessioni.php
@@ -1,14 +1,27 @@
 <?php
 session_start();
+
+echo "<pre>";
+var_dump($_SESSION);
+echo "</pre>";
+
+if (!isset($_SESSION['name'])) {
+    $_SESSION['name'] = "Mario";
+}
+
 if (!isset($_SESSION['count'])) {
     $_SESSION['count'] = 0;
 } else {
     $_SESSION['count']++;
 }
 
-echo "Hai visitato questa pagina " . $_SESSION['count'] . ' volte';
+echo "Ti chiami " . $_SESSION['name'] . " e hai visitato questa pagina " . $_SESSION['count'] . ' volte';
 
-if ($_SESSION['count'] > 4) {
+if (($_SESSION['count'] > 2) && ($_SESSION['name'] == 'Mario')) {
+    echo "Hai visitato questa pagina troppe volte. Cancello il conteggio";
+    unset($_SESSION['count']);
+    $_SESSION['name'] = "Martina";
+} elseif ($_SESSION['count'] > 4) {
     echo "Hai visitato questa pagina troppe volte. Azzero il conteggio";
     session_destroy();
 }
