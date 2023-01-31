8b3474b041ce4e8168c05d999b0ce24cfd63e298
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-01-31 10:35:19 +0100

    cookie

--- lezione6/sessioni.php
+++ lezione6/sessioni.php
@@ -3,6 +3,7 @@ session_start();
 
 echo "<pre>";
 var_dump($_SESSION);
+print_r($_COOKIE);
 echo "</pre>";
 
 if (!isset($_SESSION['name'])) {
@@ -15,14 +16,29 @@ if (!isset($_SESSION['count'])) {
     $_SESSION['count']++;
 }
 
+if (isset($_COOKIE['count'])) {
+    $count = $_COOKIE['count'] + 1;
+} else {
+    $count = 1;
+}
+
+setcookie('count', $count, time() + 3600);
+
+if (isset($_COOKIE["count"])) {
+    echo "Conteggio nel cookie " . $_COOKIE["count"] . '<br>';
+}
+
+
+
 echo "Ti chiami " . $_SESSION['name'] . " e hai visitato questa pagina " . $_SESSION['count'] . ' volte';
-echo "<br>Il tuo session id è ".session_id() ." e il tuo session name è ".session_name() . " il tuo stato è ".session_status();
+echo "<br>Il tuo session id è " . session_id() . " e il tuo session name è " . session_name() . " il tuo stato è " . session_status();
 
 if (($_SESSION['count'] > 2) && ($_SESSION['name'] == 'Mario')) {
     echo "Hai visitato questa pagina troppe volte. Cancello il conteggio";
     unset($_SESSION['count']);
     $_SESSION['name'] = "Martina";
 } elseif ($_SESSION['count'] > 4) {
-    echo "Hai visitato questa pagina troppe volte. Azzero il conteggio";
+    echo "Hai visitato questa pagina troppe volte. Azzero il conteggio e cancello i cookie";
     session_destroy();
+    setcookie("count", "", time() - 3600);
 }
