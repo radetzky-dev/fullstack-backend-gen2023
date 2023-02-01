585d791fbcf6dc3e6c001f71a1ca375a2ed82bd6
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-01 14:23:06 +0100

    todo

--- shopstore/index.php
+++ shopstore/index.php
@@ -8,6 +8,9 @@ require_once "inc/navbar.php";
     <p>Ciao, <?php echo $_SESSION["userInfo"]; ?> </p>
     <h2 class="pb-2 border-bottom">Le nostre offerte </h2>
 
+    <p>TODO 1)creare un utente admin che possa inserire \modifcare il catalogo 
+       <br> 2)il catalogo va portato in json e non come ora.
+    </p>
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
         <div class="col">
             <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-image: url('unsplash-photo-1.jpg');">
