b240424e0c8fc14d0b45a7eba1b120cbf3d8ee8c
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-09 09:56:18 +0100

    db

--- lezione14/testpdo.php
+++ lezione14/testpdo.php
@@ -1,3 +1,14 @@
 <?php
+putenv("DB_PASSWORD=");
 
-phpinfo();
+try {
+    $hostname = "localhost";
+    $dbname = "musadbshop";
+    $user = "root";
+    $db = new PDO("mysql:host=$hostname;dbname=$dbname", $user, getenv("DB_PASSWORD"));
+} catch (PDOException $e) {
+    echo "Errore: " . $e->getMessage();
+    die();
+}
+
+echo "Connessione avvenuta con successo!";
