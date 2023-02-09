69e45a7e6f6899ab12cac9112384b4b0e78fab66
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-09 10:35:23 +0100

    test statement

--- lezione14/testpdo.php
+++ lezione14/testpdo.php
@@ -5,21 +5,38 @@ putenv("DB_NAME=musadbshop");
 putenv("DB_USER=root");
 putenv("DB_PASSWORD=");
 
-
 function pdoConnect()
 {
     try {
         return new PDO("mysql:host=" . getenv('DB_HOSTNAME') . ";dbname=" . getenv('DB_NAME') . "", getenv("DB_USER"), getenv("DB_PASSWORD"));
     } catch (PDOException $e) {
         echo "Errore di connessione al DB: " . $e->getMessage();
-        die();
     }
 }
 
 $db = pdoConnect();
+
 if ($db) {
-    echo "Connessione avvenuta con successo!";
+    try {
+        //DA ESTRAPOLARE
+        $query = 'SELECT * from costumers';
+        $dbStatement = $db->prepare($query, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+        $dbStatement->execute();
+        echo "Query lanciata con successo";
+
+    } catch (PDOException $e) {
+        echo "Si è verificato un errore. Impossibile procedere " . $e->getMessage();
+    }
+
+
+    // $sth->execute(['calories' => 150, 'colour' => 'red']);
+    // $red = $sth->fetchAll();
 
     //Disconnect
-    $db = null;
+    if ($db) {
+        $db = null;
+    }
 }
+
+
+echo "<hr><a href='#'>Torna alla home</a>";
