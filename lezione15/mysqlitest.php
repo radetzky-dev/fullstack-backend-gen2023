ff8e60c8cd61057ef122d2835282ae6b5da5fdaf
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-13 10:10:41 +0100

    mysqli

---
+++ lezione15/mysqlitest.php
@@ -0,0 +1,47 @@
+<?php
+putenv("DB_HOSTNAME=localhost");
+putenv("DB_NAME=musadbshop");
+putenv("DB_USER=root");
+putenv("DB_PASSWORD=");
+
+/**
+ * getDbConnection
+ *
+ * @return mysqli
+ */
+function getDbConnection(): mysqli | null
+{
+    try {
+        return new mysqli(getenv('DB_HOSTNAME'), getenv('DB_USER'),  getenv('DB_PASSWORD'),  getenv('DB_NAME'));
+    } catch (Exception $e) {
+        echo "Impossibile connettersi al db " . $e->getMessage();
+        return null;
+    }
+}
+
+/**
+ * dbDisconnection
+ *
+ * @param  mixed $db
+ * @return bool
+ */
+function dbDisconnection(mysqli $db): bool
+{
+    try {
+        return $db->close();
+    } catch (Exception $e) {
+        echo "Impossibile disconnettersi al db " . $e->getMessage();
+        return false;
+    }
+}
+
+
+$dbConnection = getDbConnection();
+
+if ($dbConnection) {
+    echo "Connessione avvenuta con successo!<br>";
+
+    //query ecc
+
+    echo "Disconnessione dal db..." . dbDisconnection($dbConnection);
+}
