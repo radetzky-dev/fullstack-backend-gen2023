b25adb6234569584023d2f61190610371876b98a
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-15 11:20:36 +0100

    guzzle

--- shopstore/test.php
+++ shopstore/test.php
@@ -2,5 +2,23 @@
 require 'vendor/autoload.php';
 
 use Carbon\Carbon;
+use GuzzleHttp;
 
 printf("Sono le %s", Carbon::now());
+
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
+
+echo $response->getStatusCode(); // 200
+echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
+echo "<hr>";
+
+// Send an asynchronous request.
+$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo 'I completed! ' . $response->getBody();
+});
+
+$promise->wait();
