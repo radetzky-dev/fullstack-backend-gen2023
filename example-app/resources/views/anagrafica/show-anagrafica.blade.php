78475b280ff3b263709f6f252d448a7dc63453fa
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-17 10:11:48 +0100

    last

--- example-app/resources/views/anagrafica/show-anagrafica.blade.php
+++ example-app/resources/views/anagrafica/show-anagrafica.blade.php
@@ -3,7 +3,14 @@
 <head>
     <title>Mostra anagrafica</title>
 
+    @if (env('APP_ENV') == "local")
+    <!-- //npm run dev -->
     @vite('resources/js/app.js')
+    @else
+    <link rel="stylesheet" type="text/css" href="<?php echo env('ASSET_URL'); ?>app.css" />
+    <script src="{{env('ASSET_URL')}}app.js"></script>
+    @endif
+
 
 </head>
 
