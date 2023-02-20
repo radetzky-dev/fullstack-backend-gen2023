02463c45a3d29120ffbb55492c6b7f7b72852362
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-20 12:06:21 +0100

    test blade

--- example-app/resources/views/hello/saluta.blade.php
+++ example-app/resources/views/hello/saluta.blade.php
@@ -1,12 +1,22 @@
 <html>
+
     <body>
         <p>{{ $francese }}</p>
         <p>{{ $italiano }}</p>
+        <p>Solo le ore UNIX {{time()}}</p>
+        <p>Saluta {{SayHi();}}</p>
+        <p>Oggi {{date("F j, Y, g:i a")}}</p>
 
       
         <?php echo "PHP francese ". $francese;?>
         <?php echo "PHP italiano ". $italiano;?>
 
+        <?php
+        function SayHi()
+        {
+            return "Ciao mondo";
+        }
+        ?>
         
     </body>
 </html>
