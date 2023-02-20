f44ad1526aee5b9216109782799eb949fc30cc3c
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-20 09:50:33 +0100

    hellocontroller

---
+++ example-app/app/Services/HelloService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class HelloService
+{
+    public static function sayHello()
+    {
+        return "Hello world";
+    }
+
+    public function sayCiao()
+    {
+        return "Ciao";
+    }
+}
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MyTestController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\CompanyController;
-
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,6 @@ Route::get('/ciao', function (Request $request) {
     $myarray = $request->query();
     //to do
     return "ok";
-
 });
 
 Route::view('/provaparametro', 'prova', ['name' => 'Paolo']);
@@ -45,20 +44,20 @@ Route::redirect("/ciaomondo", "/ciao");
 
 
 Route::get('/username/{name}', function (string $name) {
-    return 'Questo username è valido : '.$name;
+    return 'Questo username è valido : ' . $name;
 })->where('name', '[A-Za-z]+');
 
 Route::get('/users/{name}/{number}', function ($name, $number) {
-    return 'Questo username è valido : '.$name . " num ".$number;
-})->where(['name' => '[A-Za-z]+','number' => '[0-9]+']);
+    return 'Questo username è valido : ' . $name . " num " . $number;
+})->where(['name' => '[A-Za-z]+', 'number' => '[0-9]+']);
 
 
 Route::get('/posts/{post}/comments/{comment}', function (string $mario, string $commentId) {
-    return 'Il tuo id post è '.$mario. " il commento id è ".$commentId;
+    return 'Il tuo id post è ' . $mario . " il commento id è " . $commentId;
 });
 
-Route::get('/hello-controller/', [MyTestController::class,'greeting']);
-Route::get('/show-query/', [MyTestController::class,'showquery']);
+Route::get('/hello-controller/', [MyTestController::class, 'greeting']);
+Route::get('/show-query/', [MyTestController::class, 'showquery']);
 
 //Anagrafica
 Route::controller(MyTestController::class)->group(function () {
@@ -72,7 +71,7 @@ Route::controller(MyTestController::class)->group(function () {
 Route::get('/users/{name?}', function (string $name = null) {
     return $name;
 });
- 
+
 Route::get('/users/{name?}', function (string $name = 'John') {
     return $name;
 });
@@ -80,16 +79,16 @@ Route::get('/users/{name?}', function (string $name = 'John') {
 Route::get('/user/{id}/{name}', function (string $id, string $name) {
     return "Il mio $id e il mio $name";
 })->whereNumber('id')->whereAlpha('name');
- 
+
 Route::get('/usernames/{name}', function (string $name) {
     return "Il mio $name";
 })->whereAlphaNumeric('name');
- 
+
 //5ace9ab9-e9cf-4ec6-a19d-5881212a452c
 Route::get('/useruuid/{id}', function (string $id) {
     return "Il mio $id";
 })->whereUuid('id');
- 
+
 Route::get('/category/{category}', function (string $category) {
     return "La categoria scelta fra le ammesse è $category";
 })->whereIn('category', ['movie', 'song', 'painting']);
@@ -109,4 +108,5 @@ Route::fallback(function () {
 
 Route::resource('companies', CompanyController::class);
 
-Route::get('/transistor', [CompanyController::class,'transistor'])->name("transistor");
+Route::get('/hellocontroller/', [HelloController::class, 'index']);
+
