d809d6bf9b84c35ac9a38caa49315ccae9936fdc
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-21 11:12:05 +0100

    blade

--- example-app/resources/views/test/test.blade.php
+++ example-app/resources/views/test/test.blade.php
@@ -1,13 +1,38 @@
 <?php
 $record = 5;
-
+$reco = '';
+$emptyArray = [];
 
 ?>
 
 <html>
-
 <body>
     <p>{{ $nome }}</p>
-    <p>{{ $cognome }}</p>
+    @if ($cognome != '')
+        Il cognome è <p>{{ $cognome }}</p>
+    @endif
+
+    @unless($nome == '')
+        {{--  inverso di if --}}
+        {{ $nome }}
+    @endunless
+
+    @isset($nome)
+        nome è settato
+    @endisset
+
+    @isset($xxx)
+        xxx è settato //non lo mostra non settato
+    @endisset
+
+    @empty($reco)
+        reco è vuoto
+    @endempty
+
+    @empty($emptyArray)
+        array è vuoto
+    @endempty
+
 </body>
+
 </html>
