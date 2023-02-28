5af5cd007c912e80266ce268e965fafb99afeec2
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-28 13:18:55 +0100

    prodotto

--- progettofinale/app/Http/Controllers/ProductController.php
+++ progettofinale/app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $data['products'] = Product::orderBy('id', 'desc')->paginate(15);
+        $data['products'] = Product::orderBy('id', 'desc')->paginate(9);
         return view('home.products', $data);
     }
 
--- progettofinale/resources/views/home/products.blade.php
+++ progettofinale/resources/views/home/products.blade.php
@@ -1,48 +1,31 @@
 @extends('layouts.app-master')
 
 @section('content')
-    <div class="container mt-2">
-        <div class="row">
-            <div class="col-lg-12 margin-tb">
-                <div class="pull-left">
-                    <h2>Prodotti</h2>
-                </div>
-                @auth
-                <div class="pull-right mb-2">
-                    <a class="btn btn-success" href="{{ route('products.create') }}"> Crea nuovo prodotto</a>
-                </div>
-                @endauth
-            </div>
-        </div>
-        @if ($message = Session::get('success'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
-            </div>
-        @endif
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
 
-        @if ($message = Session::get('error'))
-            <div class="alert alert-success">
-                <p>{{ $message }}</p>
+        @foreach ($products as $product)
+            <div class="col">
+                <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
+                    style="background-image: url('unsplash-photo-1.jpg');">
+                    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                        <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{{ $product->name }}</h2>
+                        <h3>€ {{ $product->price }} </h3>
+                        <img src="{{ $product->image }}" />
+                        <ul class="d-flex list-unstyled mt-auto">
+                            
+                            <p> {{ $product->description }} </p>
+                            <li class="d-flex align-items-center me-3">
+                                
+                                <small>INSERISCI NEL CARRELLO </small>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
             </div>
-        @endif
-        <table class="table table-bordered">
-            <tr>
-                <th>Id</th>
-                <th>Prodotto</th>
-                <th>Descrizione</th>
-                <th>Immagine</th>
-                <th width="280px">Action</th>
-            </tr>
-            @foreach ($products as $product)
-                <tr>
-                    <td>{{ $product->id }}</td>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->price }}</td>
-                    <td><img src="{{ $product->image }}"/></td>
-                </tr>
-            @endforeach
-        </table>
-        {!! $products->links() !!}
+        @endforeach
+
     </div>
+
+
 @endsection
