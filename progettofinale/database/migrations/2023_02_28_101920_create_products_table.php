5d76d3a39b3a68312a5094e208f1933ffb9bea25
radetzky-dev <52538594+radetzky-dev@users.noreply.github.com>
2023-02-28 11:41:40 +0100

    migrazioni

---
+++ progettofinale/database/migrations/2023_02_28_101920_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->text("description");
+            $table->double("price", 8, 2);
+            $table->integer("quantity");
+            $table->string("image");
+            //inserisco un FK su category_id
+            $table->foreignId('category_id')->constrained('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
